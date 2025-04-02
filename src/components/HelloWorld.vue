4f8ce2733313cb6f51746da1be2275d822790c7e
kyewu <18336768355@163.com>
2025-04-02 22:07:30 +0800

    add iconify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,8 @@
   <div class="color-yellow">
     <!-- -->
     {{ msg }}
+    <div class="i-ph-anchor-simple-thin hover:color-rose cursor-pointer" />
+    <div class="i-mdi-account-circle-outline color-red"></div>
   </div>
 </template>
 
